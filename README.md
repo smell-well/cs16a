c34e2c6d62726d110046d3d744d288092a27d1a5
YMK <mengke.yang@Outlook.com>
2023-04-21 17:44:58 +0800

    lab00-03

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # cs16a
 自学cs16a的最速传说
 抓紧变强
+
+- [x] lab00
+- [x] lab01
+- [x] lab02
+- [x] lab03
+- [ ] lab04
+- [ ] lab05
+- [ ] lab06
+- [ ] lab07
+- [ ] lab08
+- [ ] lab09
+- [ ] lab10
+- [ ] lab11
+- [ ] lab12
+- [ ] lab13
+- [ ] lab14
---
+++ lab01/lab01.py
@@ -0,0 +1,70 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    i = k
+    a = n
+    res = 1
+    while i:
+        res *= a
+        i -= 1
+        a -= 1
+    return res
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    s = str(y)
+    res = 0
+    for ch in s:
+        res += int(ch) - int('0')
+    return res
+
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    s = str(n)
+    cnt = 0
+    for ch in s:
+        if ch == '8':
+            cnt += 1
+
+    return cnt == 2
+
+
